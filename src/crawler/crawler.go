871e8066b76e10e2a34306f66ab627189557a373
zjucx <zju.chx@gmail.com>
2016-10-19 23:07:37 +0800

    add scrawler folder

--- src/crawler/crawler.go
+++ src/crawler/crawler.go
@@ -4,3 +4,21 @@ import (
   "fmt"
   "github.com/opesun/goquery"
 )
+
+type crawler struct {
+  runStatus bool
+  urlChan chan []string
+}
+
+func (c *crawler)doCrawl(url string) {
+  // login in weibo
+
+  for c.runStatus {
+    select {
+    case x := urlChan
+      //do something
+    default:
+    //do something
+    }
+  }
+}
