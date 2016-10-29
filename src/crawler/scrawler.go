eaef31e8242758c067ae67f2bda3717cefb23336
zjucx <zju.chx@gmail.com>
2016-10-29 22:35:09 +0800

    finish segment using jieba

--- src/crawler/scrawler.go
+++ src/crawler/scrawler.go
@@ -14,13 +14,11 @@ var mstartUrl = "http://d.weibo.com/1087030002_2975_1003_0"
 func Scrawler(username, passwd string){
   // get login cookies
   loginCookies := WeiboLogin(username, passwd)
+
   mUrl := make(map[string]string)
   for i := 1; i < 168; i++ {
     mstartUrl = "http://d.weibo.com/1087030002_2975_1003_0?pids=Pl_Core_F4RightUserList__4&page=" + strconv.Itoa(i) + "#Pl_Core_F4RightUserList__4"
     mstartResp, _ := DoRequest(`GET`, mstartUrl, ``, loginCookies, ``, header)
-
-    // get
-
     //fmt.Println(mstartResp)
     //resp make '\' as string so we shou add '\\' in regex and one blank become two blanks so be carefully
     //the regex should reg  the string of mstartResp
@@ -28,17 +26,36 @@ func Scrawler(username, passwd string){
     arrStart := reg.FindAllStringSubmatch(mstartResp, -1)
 
     if len(arrStart) > 0 {
-      fmt.Println(len(arrStart))
       for i := 0; i < len(arrStart); i++ {
-        mUrl[arrStart[i][3]] = strings.Replace(arrStart[i][2], "\\/","/", -1)
-        fmt.Println(strings.Replace(arrStart[i][2], "\\/","/", -1) + arrStart[i][3])
+        mUrl[arrStart[i][3]] = strings.Replace(strings.Replace(arrStart[i][2], "\\/", "/", -1), "weibo.com", "weibo.cn", -1)
+        fmt.Println(mUrl[arrStart[i][3]])
       }
     }
   }
-  writeMaptoFile(mUrl, "./mstarturlname.map")
+
+  for k, v := range mUrl {
+    getPageData("./data/" + k, v, loginCookies)
+  }
+  writeMaptoFile(mUrl, "./data/mstarturlname.map")
 }
 
 func writeMaptoFile(m map[string]string, filePath string) error {
+  f, err := os.Create(filePath)
+  if err != nil {
+    fmt.Printf("create map file error: %v\n", err)
+    return err
+  }
+  defer f.Close()
+
+  w := bufio.NewWriter(f)
+  for k, v := range m {
+    lineStr := fmt.Sprintf("%s^%s", k, v)
+    fmt.Fprintln(w, lineStr)
+  }
+  return w.Flush()
+}
+
+func getPageData(filePath, startUrl, loginCookies string) error {
   f, err := os.Create(filePath)
   if err != nil {
           fmt.Printf("create map file error: %v\n", err)
@@ -47,9 +64,22 @@ func writeMaptoFile(m map[string]string, filePath string) error {
   defer f.Close()
 
   w := bufio.NewWriter(f)
-  for k, v := range m {
-          lineStr := fmt.Sprintf("%s^%s", k, v)
-          fmt.Fprintln(w, lineStr)
+  //for k, v := range m {
+  mstartResp, _ := DoRequest(`GET`, startUrl, ``, loginCookies, ``, header)
+  reg := regexp.MustCompile(`<span class="ctt">(.*?)</span>`)
+  arrStart := reg.FindAllStringSubmatch(mstartResp, -1)
+  if len(arrStart) > 0 {
+    fmt.Println(len(arrStart))
+    for i := 0; i < len(arrStart); i++ {
+       //去除所有尖括号内的HTML代码，并换成换行符
+      re, _ := regexp.Compile("\\<[\\S\\s]+?\\>")
+      arrStart[i][1] = re.ReplaceAllString(arrStart[i][1], "")
+      fmt.Println(arrStart[i][1])
+      fmt.Fprintln(w, arrStart[i][1])
+    }
+    //}
+    //lineStr := fmt.Sprintf("%s^%s", k, v)
+    //fmt.Fprintln(w, lineStr)
   }
   return w.Flush()
 }
---
+++ src/segment/segment.go
@@ -0,0 +1,57 @@
+package segment
+
+import (
+  "fmt"
+  "os"
+  "path/filepath"
+  "bufio"
+  "io"
+  "strings"
+  "github.com/yanyiwu/gojieba"
+  "regexp"
+)
+
+func Segment(){
+  getFilelist("./tmp", segLine)
+}
+
+func getFilelist(path string, sego func(string)) {
+  err := filepath.Walk(path, func(path string, fi os.FileInfo, err error) error {
+    if ( fi == nil ) {return err}
+    if fi.IsDir() {return nil}
+    f, err := os.Open(path)
+    if err != nil {return err}
+    buf := bufio.NewReader(f)
+    for {
+      line, err := buf.ReadString('\n')
+      //去除非中文字符
+      reg := regexp.MustCompile("[^\u4e00-\u9fa5]")
+      line = reg.ReplaceAllString(line, "")
+      //jieba分词
+      sego(line)
+
+      if err != nil {
+  			if err == io.EOF {
+  				return nil
+  			}
+  			return err
+  		}
+    }
+  })
+  if err != nil {
+    fmt.Printf("filepath.Walk() returned %v\n", err)
+  }
+}
+
+func segLine(line string) {
+  var words []string
+  use_hmm := true
+  x := gojieba.NewJieba()
+  defer x.Free()
+  words = x.Cut(line, use_hmm)
+  //fmt.Println(line)
+  fmt.Println("精确模式:", strings.Join(words, "/"))
+  for i, v := range words {
+    //build dictory
+  }
+}
