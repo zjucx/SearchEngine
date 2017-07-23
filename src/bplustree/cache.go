cec995f5e43c469bd71a981b5ae5c05aef8fdd01
zjucx <862575451@qq.com>
2017-07-23 23:38:46 +0800

    pager cache

--- src/bplustree/cache.go
+++ src/bplustree/cache.go
@@ -1,13 +1,94 @@
 /*
-** This file implements the default page cache implementation (the
-** sqlite3_pcache interface). It also contains part of the implementation
-** of the SQLITE_CONFIG_PAGECACHE and sqlite3_release_memory() features.
-** If the default page cache implementation is overridden, then neither of
-** these two features are available.
+** A complete page cache is an instance of this structure.  Every
+** entry in the cache holds a single page of the database file.  The
+** btree layer only operates on the cached copy of the database pages.
 **
-** A Page cache line looks like this:
+** A page cache entry is "clean" if it exactly matches what is currently
+** on disk.  A page is "dirty" if it has been modified and needs to be
+** persisted to disk.
 **
-**  -------------------------------------------------------------
-**  |  database page content   |  PgHdr1  |  MemPage  |  PgHdr  |
-**  -------------------------------------------------------------
+** pDirty, pDirtyTail, pSynced:
+**   All dirty pages are linked into the doubly linked list using
+**   PgHdr.pDirtyNext and pDirtyPrev. The list is maintained in LRU order
+**   such that p was added to the list more recently than p->pDirtyNext.
+**   PCache.pDirty points to the first (newest) element in the list and
+**   pDirtyTail to the last (oldest).
 */
+
+struct PCache {
+  PgHdr *pDirty, *pDirtyTail;         /* List of dirty pages in LRU order */
+  PgHdr *pSynced;                     /* Last synced page in dirty page list */
+  int nRefSum;                        /* Sum of ref counts over all pages */
+  int szCache;                        /* Configured cache size */
+  int szSpill;                        /* Size before spilling occurs */
+  int szPage;                         /* Size of every page in this cache */
+  int szExtra;                        /* Size of extra space for each page */
+  u8 bPurgeable;                      /* True if pages are on backing store */
+  u8 eCreate;                         /* eCreate value for for xFetch() */
+  int (*xStress)(void*,PgHdr*);       /* Call to try make a page clean */
+  void *pStress;                      /* Argument to xStress */
+  sqlite3_pcache *pCache;             /* Pluggable cache module */
+};
+
+/* Initialize and shutdown the page cache subsystem */
+int cacheInitialize(void);
+void cacheShutdown(void);
+
+/* Page cache buffer management:
+** These routines implement SQLITE_CONFIG_PAGECACHE.
+*/
+void cacheBufferSetup(void *, int sz, int n);
+
+/* Create a new pager cache.
+** Under memory stress, invoke xStress to try to make pages clean.
+** Only clean and unpinned pages can be reclaimed.
+*/
+int cacheOpen(
+  int szPage,                    /* Size of every page */
+  int szExtra,                   /* Extra space associated with each page */
+  int bPurgeable,                /* True if pages are on backing store */
+  int (*xStress)(void*, PgHdr*), /* Call to try to make pages clean */
+  void *pStress,                 /* Argument to xStress */
+  PCache *pToInit                /* Preallocated space for the PCache */
+);
+
+/* Modify the page-size after the cache has been created. */
+int cacheSetPageSize(PCache *, int);
+
+/* Return the size in bytes of a PCache object.  Used to preallocate
+** storage space.
+*/
+int cacheSize(void);
+
+/* One release per successful fetch.  Page is pinned until released.
+** Reference counted.
+*/
+sqlite3_pcache_page *cacheFetch(PCache*, Pgno, int createFlag);
+int cacheFetchStress(PCache*, Pgno, sqlite3_pcache_page**);
+PgHdr *cacheFetchFinish(PCache*, Pgno, sqlite3_pcache_page *pPage);
+void cacheRelease(PgHdr*);
+
+void cacheDrop(PgHdr*);         /* Remove page from cache */
+void cacheMakeDirty(PgHdr*);    /* Make sure page is marked dirty */
+void cacheMakeClean(PgHdr*);    /* Mark a single page as clean */
+void cacheCleanAll(PCache*);    /* Mark all dirty list pages as clean */
+
+/* Get a list of all dirty pages in the cache, sorted by page number */
+PgHdr *cacheDirtyList(PCache*);
+
+/* Reset and close the cache object */
+void cacheClose(PCache*);
+
+/* Clear flags from pages of the page cache */
+void cacheClearSyncFlags(PCache *);
+
+/* Discard the contents of the cache */
+void cacheClear(PCache*);
+/* Free up as much memory as possible from the page cache */
+void cacheShrink(PCache*);
+
+/* Return the header size */
+int sqlite3HeaderSizePcache(void);
+
+/* Number of dirty pages as a percentage of the configured cache size */
+int cachePercentDirty(PCache*);
--- src/bplustree/pager.go
+++ src/bplustree/pager.go
@@ -1,4 +1,5 @@
-/* Each btree pages is divided into three sections:  The header, the
+/*
+** Each btree pages is divided into three sections:  The header, the
 ** cell pointer array, and the cell content area.  Page 1 also has a 100-byte
 ** file header that occurs before the page header.
 **
@@ -21,10 +22,73 @@
 ** The page headers looks like this:
 **
 **   OFFSET   SIZE     DESCRIPTION
-**      0       1      Flags. 1: interpage, 2: leafpage, 4: overflowpage, 8: zeropage
+**      0       1      Flags. 1: interpage, 2: leafpage, 4: overflowpage
 **      1       2      byte offset to the first freeblock
 **      3       2      number of cells on this page
 **      5       2      first byte of the cell content area
 **      7       1      number of fragmented free bytes
 **      8       4      Right child (the Ptr(N) value).  Omitted on leaves.
 */
+
+/*
+** Every page in the cache is controlled by an instance of the following
+** structure.
+**
+** A Page cache line looks like this:
+**
+**  --------------------------------------------------
+**  |  database page content   |  PgHdr  |  MemPage  |
+**  --------------------------------------------------
+*/
+struct PgHdr {
+  void *pData;                   /* Page data */
+  void *pExtra;                  /* Extra content */
+  PCache *pCache;                /* PRIVATE: Cache that owns this page */
+  PgHdr *pDirty;                 /* Transient list of dirty sorted by pgno */
+  Pager *pPager;                 /* The pager this page is part of */
+  Pgno pgno;                     /* Page number for this page */
+  i16 nRef;                      /* Number of users of this page */
+  PgHdr *pDirtyNext;             /* Next element in list of dirty pages */
+  PgHdr *pDirtyPrev;             /* Previous element in list of dirty pages */
+};
+
+struct Pager {
+  int nMmapOut;               /* Number of mmap pages currently outstanding */
+  PgHdr *pMmapFreelist;       /* List of free mmap page headers (pDirty) */
+  /*
+  ** End of the routinely-changing class members
+  ***************************************************************************/
+
+  u16 nExtra;                 /* Add this many bytes to each in-memory page */
+  int pageSize;               /* Number of bytes in a page */
+  Pgno mxPgno;                /* Maximum allowed size of the database */
+  char *zFilename;            /* Name of the database file */
+  PCache *pPCache;            /* Pointer to page cache object */
+};
+
+/* Open and close a Pager connection. */
+int pagerOpen(
+  sqlite3_vfs*,
+  Pager **ppPager,
+  const char*,
+  int,
+  int,
+  int,
+  void(*)(DbPage*)
+);
+int pagerClose(Pager *pPager, sqlite3*);
+int pagerReadFileheader(Pager*, int, unsigned char*);
+void pagerShrink(Pager*);
+
+/* Operations on page references. */
+int pagerWrite(DbPage*);
+void pagerDontWrite(DbPage*);
+int pagerMovepage(Pager*,DbPage*,Pgno,int);
+int pagerPageRefcount(DbPage*);
+void *pagerGetData(DbPage *);
+void *pagerGetExtra(DbPage *);
+
+/* Functions used to manage pager transactions and savepoints. */
+void pagerPagecount(Pager*, int*);
+int pagerCommit(Pager*,const char *zMaster, int);
+int pagerSync(Pager *pPager, const char *zMaster);
